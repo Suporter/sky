3fe1a44a89b4fc77b38dfc98764977c40ba033ac
pollonet <30765345+pollonet@users.noreply.github.com>
2019-05-04 00:30:13 +0700

    Add files via upload

---
+++ sky.js
@@ -0,0 +1,6 @@
+<div class="video-responsive">
+<video id="hls-m3u" poster="">
+    <source src="https://ul.cdn574.net:8443/hls/8u8acv17o.m3u8?s=W01u71gwgYbtdc4gcb3p4w&e=1556925091" title="Server 1" type="application/x-mpegURL"/>
+    <source src="https://ul.cdn574.net:8443/hls/8u8acv17o.m3u8?s=W01u71gwgYbtdc4gcb3p4w&e=1556925091" title="Server 2" type="application/x-mpegURL"/>
+</video>
+</div>
